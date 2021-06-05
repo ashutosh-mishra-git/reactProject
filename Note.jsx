28e92ae2f25579527ba4e69a2137e605f96e4f03
ashutosh-mishra-git <85409492+ashutosh-mishra-git@users.noreply.github.com>
2021-06-05 23:55:00 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Note() {
+  return (
+    <div className="note">
+      <h1>javaScript with Reactjs</h1>
+      <p>is it funny its scam </p>
+    </div>
+  );
+}
+export default Note;
